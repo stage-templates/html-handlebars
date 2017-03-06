f172c2c278162bceb02cf4573624c814ef665a4c
Steven Klein <steven.p.klein@gmail.com>
2017-03-06 17:30:41 -0600

    various updates for a better initial setup - move from copy to cpx

--- template/resources/data/index.js
+++ template/resources/data/index.js
@@ -2,8 +2,8 @@ var package = require('./../../package.json');
 
 module.exports = {
     "_comment": "Application data made available to handlebars.",
-    "name": "bs.dev",
-    "description": "Flat HTML Handlerbars site.",
+    "name": "{{ name }}",
+    "description": "{{ description }}",
     "version": package.version || "",
     "environment": process.env.NODE_ENV || "development",
     "production": (process.env.NODE_ENV === "production")
